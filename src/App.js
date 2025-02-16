be4d2cda4620ca07d1523d2871f884ab10b34122
Shibaz98 <shibaz98@gmail.com>
2025-02-16 13:51:56 +0000

    setting up git hub

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class='flex items-center mx-10 border-s-purple-700'>
+      <h1 class='text-3xl font-bold text-fuchsia-700'>React App</h1> 
+      <h2 class='p-30 border-s-black border-solid'>Test</h2>
+    </div>
+  );
+}
+
+export default App;
