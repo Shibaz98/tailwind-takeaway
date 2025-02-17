18890279a1e5559a32df90a7cdf53ac33e8b7c4a
Shibaz98 <shibaz98@gmail.com>
2025-02-17 19:21:13 +0000

    Navbar completed, need to work on hero section next!

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -6,24 +6,37 @@ export default function Root() {
     <div className="shadow-xl">
         <header className=" flex items-center justify-between p-4">
             <img src="/images/logo.png" alt="" className="w-32 ml-4"/>
-            <nav className='m-10 text-3xl'>
-                <NavLink className='mx-4' to='/'>Home</NavLink>
-                <NavLink className='mx-4' to='contact'>Contact</NavLink>
-                <NavLink className='mx-4' to='basket'>Basket</NavLink>
+            <nav className='mt-5 text-3xl flex'>
+                <NavLink className='mx-4  flex justify-items-center items-center border-b-4 hover:border-b-blue-400' to='/'>
+                    <img src='images/home.png' className="w-6 h-6 mr-2"/>
+                    Home
+                </NavLink>
+                <NavLink className='mx-4  flex justify-items-center items-center border-b-4 hover:border-b-blue-400' to='basket'>
+                    <img src='images/shopping-basket.png' className="w-6 h-6 mr-2"/>
+                    basket
+                </NavLink>
+                <NavLink className='mx-4 flex justify-items-center items-center border-b-4 hover:border-b-blue-400' to='contact'>
+                    <img src='images/contact-mail.png' className="w-8 h-8 mr-2"/>
+                    contact
+                </NavLink>
             </nav>
         </header>
         <div className="flex justify-center">
-            <NavLink className='text-center'>
-                <img src="/images/burgers.webp" alt="image of burger" className="w-32 mx-4" />
+            <NavLink to='burgers' className='text-center border-solid border-4 border-yellow-200 rounded-xl bg-yellow-200 font-medium mb-2 h-26 my-2 mx-2'>
+                <img src="/images/burgers.webp" alt="image of burger" className="w-32 mx-3" />
                 Burgers
             </NavLink>
-            <NavLink className='text-center'>
-                <img src="/images/burgers.webp" alt="image of burger" className="w-32 mx-4" />
-                Burgers
+            <NavLink to='pizza' className='text-center border-solid border-4 border-pink-200 rounded-xl bg-pink-200 font-medium mb-2 h-26 my-2 mx-2'>
+                <img src="/images/pizza.webp" alt="image of burger" className="w-32 mx-3" />
+                Pizza
             </NavLink>
-            <NavLink className='text-center'>
-                <img src="/images/burgers.webp" alt="image of burger" className="w-32 mx-4" />
-                Burgers
+            <NavLink to='kebab' className='text-center border-solid border-4 border-indigo-200 rounded-xl bg-indigo-200 font-medium mb-2 h-26 my-2 mx-2'>
+                <img src="/images/kebabs.webp" alt="image of burger" className="w-32 mx-3" />
+                Kebab
+            </NavLink>
+            <NavLink to='sandwiches' className='text-center border-solid border-4 border-orange-200 rounded-xl bg-orange-200 font-medium mb-2 h-26 my-2 mx-2'>
+                <img src="/images/sandwiches.webp" alt="image of burger" className="w-32 mx-3" />
+                Sandwiches
             </NavLink>
         </div>
         <Outlet/>
